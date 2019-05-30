19df5575fc8b2ec1ebbd0b21c79da5688fbbe633
Tinkers Projects <williambailes@gmail.com>
2019-05-30 10:58:21 +1000

    Add files via upload

--- examples/example1/example1.ino
+++ examples/example1/example1.ino
@@ -4,7 +4,7 @@ double B1angle;
 double B2angle;
 double B3angle;
 
-DeltaInverseKinematics IPK(&B1angle,&B2angle,&B3angle,100,300,120,40);
+DeltaInverseKinematics IPK(&B1angle,&B2angle,&B3angle,0.100,0.300,0.120,0.040);
 
 void setup() 
 {
@@ -17,15 +17,15 @@ void loop()
   Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
   delay(3000);
 
-  IPK.set(0,0,30);
+  IPK.set(0,0,0.030);
   Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
   delay(3000);
 
-  IPK.set(100,100,30);
+  IPK.set(0.100,0.100,0.030);
   Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
   delay(3000);
 
-  IPK.set(-100,-100,30);
+  IPK.set(-0.100,-0.100,0.030);
   Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
   delay(3000);
 }
