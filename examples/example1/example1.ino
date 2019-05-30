d3a75be4bca61f2deb0b1a8d1f5fcce61cc94675
Tinkers Projects <williambailes@gmail.com>
2019-05-30 11:24:16 +1000

    Update example1.ino

--- examples/example1/example1.ino
+++ examples/example1/example1.ino
@@ -1,31 +1,27 @@
 #include <DeltaInverseKinematics.h>
 
-double B1angle;
-double B2angle;
-double B3angle;
+double angleB1;
+double angleB2;
+double angleB3;
 
-DeltaInverseKinematics IPK(&B1angle,&B2angle,&B3angle,0.100,0.300,0.120,0.040);
+DeltaInverseKinematics IPK(&angleB1,&angleB2,&angleB3,0.070,0.300,0.139,0.112);
 
 void setup() 
-{
+{  
   Serial.begin(115200);
 }
 
 void loop() 
 {
-  IPK.set(0,0,0);
-  Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
+  IPK.set(0,0,-0.300);  
+  Serial.println(String(angleB1)+","+String(angleB2* 180 / 3.14)+","+String(angleB3* 180 / 3.14));
   delay(3000);
 
-  IPK.set(0,0,0.030);
-  Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
+  IPK.set(0.100,0.100,-0.270);
+  Serial.println(String(angleB1)+","+String(angleB2* 180 / 3.14)+","+String(angleB3* 180 / 3.14));
   delay(3000);
 
-  IPK.set(0.100,0.100,0.030);
-  Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
-  delay(3000);
-
-  IPK.set(-0.100,-0.100,0.030);
-  Serial.println(String(B1angle)+","+String(B2angle)+","+String(B3angle));
+  IPK.set(-0.100,-0.100,-0.270);
+  Serial.println(String(angleB1)+","+String(angleB2* 180 / 3.14)+","+String(angleB3* 180 / 3.14));
   delay(3000);
 }
